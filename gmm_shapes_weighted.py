1171af18b021db123ed3909d318b5e0ca9104f7c
McCullagh Lab <martin.mccullagh@colostate.edu>
2021-06-15 11:48:59 -0500

    updated to include two version: uniform and weighted
    Also extracted traj tools and kmeans into separate files

--- gmm_shapes_weighted.py
+++ gmm_shapes_weighted.py
@@ -5,6 +5,7 @@ from numba import jit
 from sklearn import mixture
 from sklearn import metrics
 from scipy.stats import multivariate_normal
+from scipy import spatial
 import traj_tools
 import kmeans_shapes
 
@@ -14,15 +15,14 @@ gammaThresh = 1E-15
 eigenValueThresh = 1E-10
 
 #@jit
-def maximum_likelihood_opt(lnWeights,lnLikelihood,centers,trajData):
+def maximum_likelihood_opt(lnWeights,lnLikelihood,centers,trajData,covar,kabschThresh):
     # get metadata from trajectory data
     nFrames = trajData.shape[0]
     nAtoms = trajData.shape[1]
     nDim = trajData.shape[2]
     nFeatures = nDim*nAtoms
     nClusters = lnWeights.size
-    # declare cluster variances
-    var = np.empty(nClusters,dtype=np.float64)
+    # Compute the normaliztion constant and overall loglikelihood
     logLikelihood = float(0.0)
     logNorm = []
     indeces = []
@@ -52,14 +52,14 @@ def maximum_likelihood_opt(lnWeights,lnLikelihood,centers,trajData):
         # will only use frames that have greater than gammaThresh weight
         gamma_indeces = np.argwhere(gamma > gammaThresh).flatten()
         # update mean and variance
-        centers[k], var[k] = traj_tools.traj_iterative_average_var_weighted(trajData[indeces[gamma_indeces]], gamma[gamma_indeces], centers[k])
+        centers[k], covar[k] = traj_tools.traj_iterative_average_covar_weighted_weighted_kabsch(trajData[indeces[gamma_indeces]], gamma[gamma_indeces], centers[k], covar[k],thresh=kabschThresh)
         # update the weights
         lnWeights[k] = np.log(np.mean(gamma))
-    return centers, var, lnWeights, logLikelihood
+    return centers, covar, lnWeights, logLikelihood
 
 # Expectation step
 @jit
-def expectation(trajData, centers, var):
+def expectation(trajData, centers, covar):
     # meta data
     nClusters = centers.shape[0]
     nFrames = trajData.shape[0]
@@ -70,37 +70,62 @@ def expectation(trajData, centers, var):
     # compute likelihood of each frame at each Gaussian
     for k in range(nClusters):
         # align the entire trajectory to each cluster mean if requested
-        trajData = traj_tools.traj_align(trajData,centers[k])
-        lnLikelihood[k,:] = ln_spherical_gaussian_pdf(trajData.reshape(nFrames,nFeatures), centers[k].reshape(nFeatures), var[k])
+        trajData = traj_tools.traj_align_weighted_kabsch(trajData,centers[k],covar[k])
+        lnLikelihood[k,:] = ln_multivariate_NxN_gaussian_pdf(trajData.reshape(nFrames,nFeatures), centers[k].reshape(nFeatures), covar[k])
     return lnLikelihood
 
+#@jit
+def pseudo_lpdet_inv(sigma):
+    N = sigma.shape[0]
+    e, v = np.linalg.eigh(sigma)
+    precision = np.zeros(sigma.shape,dtype=np.float128)
+    lpdet = 0.0
+    for i in range(N):
+        if (e[i] > eigenValueThresh):
+            lpdet += np.log(e[i])
+            precision += 1.0/e[i]*np.outer(v[:,i],v[:,i])
+    #pdet = np.exp(lpdet)
+    return lpdet, precision
 
-@jit(nopython=True)
-def ln_spherical_gaussian_pdf(x, mu, sigma):
+def multivariate_gaussian_pdf(x, mu, sigma):
+    lpdet, precision = pseudo_lpdet_inv(sigma)
     nSamples = x.shape[0]
     nDim = x.shape[1]
-    lnnorm = -0.5*nDim*(np.log(2.0*np.pi*sigma))
-    mvG = np.empty(nSamples,dtype=np.float64)
-    multiplier = -0.5/sigma
+    norm = -0.5*(nDim*np.log(2.0*np.pi)+lpdet)
+    mvG = np.empty(nSamples,dtype=np.float128)
     for i in range(nSamples):
         diffV = x[i] - mu
-        mvG[i] = multiplier*np.dot(diffV,diffV) + lnnorm
+        exponent = -0.5*np.dot(diffV.T,np.dot(precision,diffV)) + norm
+        mvG[i] = np.exp(exponent)
+    return mvG
+
+#@jit
+def ln_multivariate_NxN_gaussian_pdf(x, mu, sigma):
+    sigma3Nx3N = np.kron(sigma,np.identity(3))
+    lpdet, precision = pseudo_lpdet_inv(sigma3Nx3N)
+    nSamples = x.shape[0]
+    nDim = x.shape[1]
+    lnnorm = -0.5*(nDim*np.log(2.0*np.pi)+lpdet)
+    mvG = np.empty(nSamples,dtype=np.float64)
+    for i in range(nSamples):
+        diffV = x[i] - mu
+        mvG[i] = -0.5*np.dot(diffV.T,np.dot(precision,diffV)) + lnnorm
     return mvG
 
 @jit(nopython=True)
-def compute_bic(nFeatures, nClusters, nFrames, logLikelihood):
-    k = nClusters*(nFeatures + 1 + 1) - 1
+def compute_bic(nAtoms, nDim, nClusters, nFrames, logLikelihood):
+    k = nClusters*(nAtoms*nDim + nAtoms*(nAtoms+1)/2 + 1) - 1
     return k*np.log(nFrames) - 2*logLikelihood
 
 @jit(nopython=True)
-def compute_aic(nFeatures, nClusters, logLikelihood):
-    k = nClusters*(nFeatures + 1 + 1) - 1
+def compute_aic(nAtoms, nDim, nClusters, logLikelihood):
+    k = nClusters*(nAtoms*nDim + nAtoms*(nAtoms+1)/2 + 1) - 1
     return 2*k - 2*logLikelihood
 
 # class
-class gmm_shape:
-    
-    def __init__(self, nClusters, logThresh=1E-3,maxSteps=50,align=True, initClusters="GMM", initIter=5):
+class gmm_shape_weighted:
+
+    def __init__(self, nClusters, logThresh=1E-3,maxSteps=50,align=True, initClusters="GMM", initIter=5, kabschThresh=1E-4):
         
         self.nClusters = nClusters
         self.logThresh = logThresh
@@ -108,7 +133,9 @@ class gmm_shape:
         self.align = align
         self.initClusters = initClusters
         self.initIter = initIter
+        self.kabschThresh = kabschThresh
 
+#    @jit(locals={ 'self.nFrames': numba.types.int_})#, 'self.nAtoms': numba.types.int_, 'self.nDim': numba.types.int_, 'self.nFeatures': numba.types.int_ })
     def fit(self,trajData):
         # get metadata from trajectory data
         self.nFrames = int(trajData.shape[0])
@@ -119,10 +146,10 @@ class gmm_shape:
         self.trajData = traj_tools.traj_iterative_average(trajData)[1]
         # declare some important arrays for the model
         self.centers = np.empty((self.nClusters,self.nAtoms,self.nDim),dtype=np.float64)
-        self.var = np.empty(self.nClusters,dtype=np.float64)
+        self.covar = np.empty((self.nClusters,self.nAtoms,self.nAtoms),dtype=np.float64)
         self.clusters = np.zeros(self.nFrames,dtype=np.int)
         self.likelihood = np.empty((self.nClusters,self.nFrames),dtype=np.float128)
-        self.weights = np.empty(self.nClusters,dtype=np.float128)
+        self.weights = np.empty(self.nClusters,dtype=np.float64)
         self.lnLikelihood = np.empty((self.nClusters,self.nFrames),dtype=np.float64)
         self.lnWeights = np.empty(self.nClusters,dtype=np.float64)
         # make initial clustering based on input user choice (default is GMM without alignment)
@@ -158,21 +185,16 @@ class gmm_shape:
             gmm = mixture.GaussianMixture(n_components=self.nClusters, max_iter=200, covariance_type='full', init_params="kmeans").fit(self.trajData.reshape(self.nFrames,self.nFeatures))
             self.clusters = gmm.predict(self.trajData.reshape(self.nFrames,self.nFeatures))
             score = gmm.score(self.trajData.reshape(self.nFrames,self.nFeatures))
-            #self.weights = gmm.weights_
-            #self.centers = gmm.means_.reshape(self.nClusters,self.nAtoms,self.nDim)
             for i in range(self.initIter-1):
                 gmm = mixture.GaussianMixture(n_components=self.nClusters, max_iter=200, covariance_type='full', init_params="kmeans").fit(self.trajData.reshape(self.nFrames,self.nFeatures))  
                 tempScore = gmm.score(self.trajData.reshape(self.nFrames,self.nFeatures))
                 if (tempScore > score):
                     self.clusters = gmm.predict(self.trajData.reshape(self.nFrames,self.nFeatures))
                     score = tempScore
-                  #  self.weights = gmm.weights_
-                  #  self.centers = gmm.means_.reshape(self.nClusters,self.nAtoms,self.nDim)
-
         # compute average and covariance of initial clustering
         for k in range(self.nClusters):
             indeces = np.argwhere(self.clusters == k).flatten()
-            self.centers[k],self.var[k] = traj_tools.traj_iterative_average_var(self.trajData[indeces])
+            self.centers[k],self.covar[k] = traj_tools.traj_iterative_average_covar_weighted_kabsch_v02(self.trajData[indeces],thresh=self.kabschThresh)
             # initialize weights as populations of clusters
             self.weights[k] = indeces.size
         self.weights /= np.sum(self.weights)
@@ -184,9 +206,9 @@ class gmm_shape:
         logLikelihoodArray = np.empty(self.maxSteps,dtype=np.float64)
         while step < self.maxSteps and logLikeDiff > self.logThresh:
             # Expectation step
-            self.lnLikelihood = expectation(self.trajData, self.centers, self.var)
+            self.lnLikelihood = expectation(self.trajData, self.centers, self.covar)
             # Maximum Likelihood Optimization
-            self.centers, self.var, self.lnWeights, logLikelihoodArray[step] = maximum_likelihood_opt(self.lnWeights, self.lnLikelihood, self.centers, self.trajData)
+            self.centers, self.covar, self.lnWeights, logLikelihoodArray[step] = maximum_likelihood_opt(self.lnWeights, self.lnLikelihood, self.centers, self.trajData, self.covar,self.kabschThresh)
             print(step, np.exp(self.lnWeights), logLikelihoodArray[step])
             # compute convergence criteria
             if step>0:
@@ -202,32 +224,31 @@ class gmm_shape:
         self.centers, self.globalCenter = traj_tools.traj_iterative_average_weighted(self.centers,self.weights)
         for k in range(self.nClusters):
             indeces = np.argwhere(self.clusters == k).flatten()
-            self.trajData[indeces] = traj_tools.traj_align(self.trajData[indeces],self.centers[k])
+            self.trajData[indeces] = traj_tools.traj_align_weighted_kabsch(self.trajData[indeces],self.centers[k],self.covar[k])
         # Compute clustering scores
         self.silhouette_score = metrics.silhouette_score(self.trajData.reshape(self.nFrames,self.nFeatures), self.clusters)
         self.ch_score = metrics.calinski_harabasz_score(self.trajData.reshape(self.nFrames,self.nFeatures), self.clusters)
         self.db_score = metrics.davies_bouldin_score(self.trajData.reshape(self.nFrames,self.nFeatures), self.clusters)
-        self.bic = compute_bic(self.nFeatures-6, self.nClusters, self.nFrames, self.logLikelihood)
-        self.aic = compute_aic(self.nFeatures-6, self.nClusters, self.logLikelihood)
+        self.bic = compute_bic(self.nAtoms, self.nDim, self.nClusters, self.nFrames, self.logLikelihood)
+        self.aic = compute_aic(self.nAtoms, self.nDim, self.nClusters, self.logLikelihood)
 
     def predict(self,trajData):
         # get metadata from trajectory data
         nFrames = trajData.shape[0]
         # declare likelihood array
-        likelihood = np.empty((self.nClusters,nFrames),dtype=np.float128)
+        lnLikelihood = np.empty((self.nClusters,nFrames),dtype=np.float64)
         # make sure trajectory is centered
         trajData = traj_tools.traj_remove_cog_translation(trajData)
         # Expectation step
         for k in range(self.nClusters):
             # align the entire trajectory to each cluster mean
-            trajData = traj_tools.traj_align(trajData,self.centers[k])
-            likelihood[k,:] = multivariate_normal.pdf(x=trajData.reshape(nFrames,self.nFeatures), mean=self.centers[k].reshape(self.nFeatures), cov=self.covar[k],allow_singular=True)
+            trajData = traj_tools.traj_align_weighted_kabsch(trajData,self.centers[k],self.covar[k])
+            lnLikelihood[k,:] = ln_multivariate_NxN_gaussian_pdf(trajData.reshape(nFrames,self.nFeatures), centers[k].reshape(self.nFeatures), covar[k])
         # assign clusters based on largest likelihood (probability density)
-        clusters = np.argmax(likelihood, axis = 0)
+        clusters = np.argmax(lnLikelihood, axis = 0)
         # center trajectory around averages
         for k in range(self.nClusters):
             indeces = np.argwhere(clusters == k).flatten()
-            trajData[indeces] = traj_tools.traj_align(trajData[indeces],self.centers[k])
+            trajData[indeces] = traj_tools.traj_align_weighted_kabsch(trajData[indeces],self.centers[k],self.covar[k])
         return clusters, trajData
-
 
